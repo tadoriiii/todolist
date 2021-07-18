a69586a18308b66fe2ac3da8438565e615881f3b
tadoriiii <tw772@naver.com>
2021-07-19 00:06:50 +0900

    using props emotion

--- src/components/InputList.js
+++ src/components/InputList.js
@@ -7,8 +7,8 @@ const InputList = ({list, onRemove, openModal}) => {
         <List key={item.id}>
             <ItemTitle>{item.title}</ItemTitle>
             <ItemContent>{item.content}</ItemContent>
-            <Remove onClick={()=>onRemove(item.id)}>삭제</Remove>
-            <Update onClick={()=>openModal(item.title, item.content, item.id)}>수정</Update>
+            <Button onClick={()=>onRemove(item.id)}>삭제</Button>
+            <Button primary onClick={()=>openModal(item.title, item.content, item.id)}>수정</Button>
         </List>
     ))
     return(
@@ -49,7 +49,7 @@ const ItemContent = styled.div`
     font-weight : 400;
 `;
 
-const Remove = styled.button`
+{/*const Remove = styled.button`
     background-color: #9EC9E2;
     border : 1px solid black;
     color: black;
@@ -68,4 +68,15 @@ const Update = styled.button`
     width:32px;
     height:32px;
     margin-left : 4px;
+`;*/}
+
+const Button = styled.button`
+    background-color: ${props => props.primary ? '#9EC9E2' : '#F5EBAD'};
+    border : 1px solid black;
+    color: black;
+    margin-bottom:2px;
+    font-size: 14px;
+    width:32px;
+    height:32px;
+    margin-left : ${props => props.primary ? '4px' : '0'};
 `;
